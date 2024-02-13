d3a247f14d825e4aeb3d18abe1104f07d3cca184
gnafhan <nafhanghifari@gmail.com>
2024-02-13 11:35:33 +0700

    knapsack

---
+++ knapsack.py
@@ -0,0 +1,25 @@
+def knapsack_light(value1, weight1, value2, weight2, max_w):
+    i1 = [value1, weight1, value1/weight1]
+    i2 = [value2, weight2, value2/weight2]
+    all = sorted([i1,i2],key=lambda x : x[2], reverse=True )
+    ttl = 0
+    idx = 0
+    isAlr= False
+    for i in all:
+        if weight1+weight2 <= max_w:
+            return value1+value2
+
+        if max_w>= i[1]:
+            if idx == 0:
+                if all[1][0] > i[0] and max_w>= all[1][1] and isAlr==False:
+                    ttl+=all[1][0]
+                    max_w-= all[1][1]
+                    isAlr=True
+                    continue
+            ttl+=i[0]
+            max_w-= i[1]
+
+    return ttl
+
+
+print(knapsack_light(39, 12, 30, 9, 41))
